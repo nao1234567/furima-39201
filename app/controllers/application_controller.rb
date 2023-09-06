7d93ace638ade3155e0a80f2767a7a761029f1a4
nao1234567 <134265757+nao1234567@users.noreply.github.com>
2023-09-06 16:28:51 +0900

    デプロイ＆Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
+  end
 end
+
+# Basic認証によるログインの要求は、すべてのコントローラーで行い。そのため、Basic認証の処理をapplication_controller.rbのprivate以下にメソッドとして定義し、before_actionで呼び出している。どのページにアクセスしてもBasic認証が要求されるようになった。
