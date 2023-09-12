935efec9711e7314b73fe64f424eed30ab62a4db
nao1234567 <134265757+nao1234567@users.noreply.github.com>
2023-09-12 18:31:40 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,7 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角の英字と数字の両方を含めて設定してください'
 
-  has_many :products
-  has_many :purchases
+  # has_many :products
+  # has_many :purchases
+  # 後での実装流れが思い出しやすいように、そして入力漏れを防ぐためにコメントをしています。
 end
