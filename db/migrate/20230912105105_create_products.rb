a6cac193f522b95a8450b679920b737bf9a69eb8
nao1234567 <134265757+nao1234567@users.noreply.github.com>
2023-09-15 22:05:39 +0900

    修正

--- db/migrate/20230912105105_create_products.rb
+++ db/migrate/20230912105105_create_products.rb
@@ -1,6 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
+
       t.references :user, null: false, foreign_key: true
       t.string :name, null: false
       t.integer :category_id, null: false
