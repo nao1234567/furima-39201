165e35c0e5f1db2c4f011b3dadc3df0cd859ad33
nao1234567 <134265757+nao1234567@users.noreply.github.com>
2023-09-19 21:21:57 +0900

    商品削除機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,7 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_product, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
   before_action :contributor_confirmation, only: [:edit, :update]
-  
 
   def index
     @products = Product.all.order(created_at: :desc)
@@ -22,19 +21,24 @@ class ProductsController < ApplicationController
   end
 
   def show
-    
   end
 
   def edit
-  
   end
 
   def update
-    
     if @product.update(product_params)
       redirect_to product_path
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if @product.destroy
+      redirect_to root_path
+    else
+      :show
     end
   end
 
@@ -46,14 +50,12 @@ class ProductsController < ApplicationController
   end
 
   def contributor_confirmation
-   unless  current_user.id == @product.user.id
+    return if current_user.id == @product.user.id
+
     redirect_to root_path
-   end
   end
 
   def set_product
     @product = Product.find(params[:id])
   end
-
-  
 end
