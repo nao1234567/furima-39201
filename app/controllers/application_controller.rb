ed58433a5838aae0998589aaca880dfd1fc8f043
nao1234567 <134265757+nao1234567@users.noreply.github.com>
2023-09-26 12:36:49 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
+
 
   private
 
@@ -8,6 +10,13 @@ class ApplicationController < ActionController::Base
                                       keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana,
                                              :birth_day])
   end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
+  end
+
+
 end
 
-# Basic認証によるログインの要求は、すべてのコントローラーで行い。そのため、Basic認証の処理をapplication_controller.rbのprivate以下にメソッドとして定義し、before_actionで呼び出している。どのページにアクセスしてもBasic認証が要求されるようになった。
